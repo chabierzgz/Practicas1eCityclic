814ec78ca78f4b92e0ff20bdf9e55ca1a4ea0253
AzureAD\CarlosTorrejon <ctorrejon@ecityclic.com>
2024-01-11 18:07:24 +0100

    dev: Añade get y set

--- src/main/java/com/ecityclic/practicas1eCityclic/beans/Persona.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/beans/Persona.java
@@ -1,5 +1,30 @@
 package com.ecityclic.practicas1eCityclic.beans;
 
+import java.util.Date;
+
 public class Persona {
+	
+	private String nom;
+	private Date fechaNacimiento;
+	private String ciutat;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getCiutat() {
+		return ciutat;
+	}
+	public void setCiutat(String ciutat) {
+		this.ciutat = ciutat;
+	}
 
 }
