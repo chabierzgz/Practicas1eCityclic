3a41dfa20971a656ddd0a6647a7225c63ee5acb8
AzureAD\CarlosTorrejon <ctorrejon@ecityclic.com>
2024-01-11 17:51:35 +0100

    dev: añade metodo sumar

--- src/main/java/com/ecityclic/practicas1eCityclic/helper/Operaciones.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/helper/Operaciones.java
@@ -1,5 +1,12 @@
 package com.ecityclic.practicas1eCityclic.helper;
 
 public class Operaciones {
+	
+	public int suma(int a, int b) {
+		int resultat;
+		
+		resultat= a+b;
+		return resultat;
+	}
 
 }
