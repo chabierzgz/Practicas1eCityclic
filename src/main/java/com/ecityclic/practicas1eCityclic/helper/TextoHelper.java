4fdc02c803f762b80bbfaf429980073583483dec
AzureAD\CarlosTorrejon <ctorrejon@ecityclic.com>
2024-01-12 16:15:48 +0100

    dev: Añade método dibujar

--- src/main/java/com/ecityclic/practicas1eCityclic/helper/TextoHelper.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/helper/TextoHelper.java
@@ -1,7 +1,17 @@
 package com.ecityclic.practicas1eCityclic.helper;
 
+import com.ecityclic.practicas1eCityclic.beans.Persona;
+
+import jdk.internal.org.jline.utils.Log;
 
 public class TextoHelper {
+	
+	public void dibujar (Persona persona) {
+		
+		Log.info("nombre:{} Fecha de nacimiento: {} ciudad: {}"
+				,persona.getNom(),persona.getFechaNacimiento(), persona.getCiutat());
+		
+	}
 
 }
 
