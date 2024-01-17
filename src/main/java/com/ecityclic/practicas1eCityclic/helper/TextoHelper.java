990c64e70560e33b604f218b53d7cbce9a360175
AzureAD\CarlosTorrejon <ctorrejon@ecityclic.com>
2024-01-17 17:24:48 +0100

    dev: Borro clase persona

--- src/main/java/com/ecityclic/practicas1eCityclic/helper/TextoHelper.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/helper/TextoHelper.java
@@ -1,21 +1,23 @@
 package com.ecityclic.practicas1eCityclic.helper;
 
-public class TextoHelper {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
-<<<<<<< Updated upstream
-=======
-	Scanner lector = new Scanner(System.in);
+import com.ecityclic.practicas1eCityclic.beans.Persona;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class TextoHelper {
 
 	public void dibujar(Persona persona, int repetir) {
 
 		for (int i = 0; i < repetir; i++) {
 
-		}
-
-		int i = 0;
-		while (i < repetir) {
 			log.info("Datos personales :" + persona);
-			i++;
+
 		}
 
 	}
@@ -24,30 +26,17 @@ public class TextoHelper {
 
 		List<Persona> personas = new ArrayList<Persona>();
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		int i = 0;
 
 		while (i < numDePersonas) {
 
 			Persona persona = new Persona();
 
-			System.out.println("escribe nombre");
-			persona.setNom("Nombre " + i);// lector.nextLine());
+			persona.setNom("Nombre " + i);
 
-			System.out.println("escribe feCHa de naCimiento (dd/MM/yyyy)");
-			String feCHaNaCimiento = lector.nextLine();
+			persona.setFechaNacimiento(generaFecha());
 
-			System.out.println("escribe ciudad");
-			String ciudad = lector.nextLine();
-
-			try {
-				persona.setFechaNacimiento(sdf.parse(feCHaNaCimiento));
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-
-			persona.setCiutat(ciudad);
+			persona.setCiutat("Ciudad " + i);
 
 			personas.add(persona);
 
@@ -61,27 +50,7 @@ public class TextoHelper {
 
 	public void pintarSoloMayoresDe(int edad, int cantidadDePersonas) {
 
-		List<Persona> personas = new ArrayList<Persona>();
-
-		int i = 0;
-
-		while (i < cantidadDePersonas) {
-
-			Persona persona = new Persona();
-
-			persona.setNom("Nombre " + i);
-
-			persona.setFechaNacimiento(generaFecha());
-
-			persona.setCiutat("Ciudad " + i);
-
-			personas.add(persona);
-
-			log.info(persona);
-
-			i++;
-
-		}
+		List<Persona> personas = getPersonas(cantidadDePersonas);
 
 		for (Persona person : personas) {
 
@@ -105,10 +74,10 @@ public class TextoHelper {
 
 	public Date generaFecha() {
 		Calendar c = Calendar.getInstance();
-		c.add(Calendar.YEAR, -((int) (Math.random() * 100)));
+		int num = (int) (Math.random() * 100);
+
+		c.add(Calendar.YEAR, -num);
 		return c.getTime();
 
 	}
-
->>>>>>> Stashed changes
 }
