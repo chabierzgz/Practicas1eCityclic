290686862e9fbf62d5ed6e01e54e6de0331fefbd
AzureAD\CarlosTorrejon <ctorrejon@ecityclic.com>
2024-01-17 17:33:13 +0100

    dev: Añade la clase persona borrada

--- src/main/java/com/ecityclic/practicas1eCityclic/Application.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/Application.java
@@ -5,61 +5,51 @@ package com.ecityclic.practicas1eCityclic;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
-import com.ecityclic.practicas1eCityclic.beans.Persona;
 import com.ecityclic.practicas1eCityclic.helper.TextoHelper;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class Application {
-	
+
 	public static void main(String[] args) {
 		Application application = new Application();
 		application.run();
 	}
-	
+
 	private void run() {
 //		int prueba = 1;
 //		log.info("Método main para hacer la prueba {}", prueba);
-		
-/*		Date fecha1 = getBirthDate1(1050,Calendar.MARCH,24);
-		Date fecha2 = getBirthDate2("24/03/1950");
-		
-		
-		Persona persona = new Persona();
-		persona.setNom("Carlos");
-		persona.setFechaNacimiento(fecha1);
-		persona.setCiutat("Mataró");
-		
+
+		/*
+		 * Date fecha1 = getBirthDate1(1050,Calendar.MARCH,24); Date fecha2 =
+		 * getBirthDate2("24/03/1950");
+		 * 
+		 * 
+		 * Persona persona = new Persona(); persona.setNom("Carlos");
+		 * persona.setFechaNacimiento(fecha1); persona.setCiutat("Mataró");
+		 * 
+		 * TextoHelper textoHelper = new TextoHelper();
+		 * 
+		 * textoHelper.dibujar(persona, 5);
+		 * 
+		 * List<Persona> personas = textoHelper.getPersonas(2);
+		 * 
+		 * for (Persona p : personas) { log.info(p); }
+		 * 
+		 * textoHelper.pintarSoloMayoresDe(40);
+		 */
+
 		TextoHelper textoHelper = new TextoHelper();
-		
-		textoHelper.dibujar(persona, 5);
-		
-		List<Persona> personas = textoHelper.getPersonas(2);
-		
-		for (Persona p : personas) {
-			log.info(p);
-		}
-		
-		textoHelper.pintarSoloMayoresDe(40);*/
-		
-		TextoHelper textoHelper = new TextoHelper();
-		
-		textoHelper.pintarSoloMayoresDe(40);
-		
-		
+
+		textoHelper.pintarSoloMayoresDe(40, 3);
 
 	}
-	
-	
 
-	private Date getBirthDate2(String fechaString){
+	private Date getBirthDate2(String fechaString) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Date fecha = null;
 		try {
@@ -69,13 +59,13 @@ public class Application {
 		}
 		return fecha;
 	}
-	
-	private Date getBirthDate1(int year, int month,  int day) {
+
+	private Date getBirthDate1(int year, int month, int day) {
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.YEAR, year);
 		c.set(Calendar.MONTH, month);
 		c.set(Calendar.DAY_OF_MONTH, day);
-		
+
 		return c.getTime();
 	}
 
