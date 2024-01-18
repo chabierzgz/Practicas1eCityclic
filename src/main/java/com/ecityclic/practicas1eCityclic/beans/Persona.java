b23422ec8f522e679da92efe9376c106cec52bf0
AzureAD\CarlosTorrejon <ctorrejon@ecityclic.com>
2024-01-18 17:23:04 +0100

    dev: Arregla clase persona

--- src/main/java/com/ecityclic/practicas1eCityclic/beans/Persona.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/beans/Persona.java
@@ -2,7 +2,6 @@ package com.ecityclic.practicas1eCityclic.beans;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import lombok.Data;
 import lombok.ToString;
 
@@ -10,22 +9,24 @@ import lombok.ToString;
 @ToString
 public class Persona {
 
+	
 	private String nom;
 	private Date fechaNacimiento;
 	private String ciutat;
-
+	
 	@Override
 	public String toString() {
 		String objeto = "\n";
-
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		objeto += "nombre: " + nom + "\n";
-		objeto += "fecha Nacimiento: " + sdf.format(fechaNacimiento) + "\n";
-		objeto += "ciudad: " + ciutat + "\n";
-
-		String mejor = String.format("\nNombre: %s\nNacimiento: %s\nCiudad: %s", nom, sdf.format(fechaNacimiento),
-				ciutat);
+		
+		objeto += "nombre: "+nom+"\n";
+		objeto += "fecha Nacimiento: "+sdf.format(fechaNacimiento)+"\n";
+		objeto += "ciudad: "+ciutat+"\n";
+		
+		
+		String mejor = String.format("\nNombre: %s\nNacimiento: %s\nCiudad: %s", nom, sdf.format(fechaNacimiento), ciutat);
 		return mejor;
 	}
+
 }
