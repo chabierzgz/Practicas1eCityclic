53d3ec1e1c7bb6b65009665d74dcf4c031484752
Chabier <cnogueras@ecityclic.com>
2024-01-19 10:43:34 +0100

    dev: Intenta que la aplicación mantenga la BD persistida sin reiniciarla cada vez

--- src/main/java/com/ecityclic/practicas1eCityclic/service/PersonService.java
+++ src/main/java/com/ecityclic/practicas1eCityclic/service/PersonService.java
@@ -2,6 +2,8 @@ package com.ecityclic.practicas1eCityclic.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,12 @@ public class PersonService {
     @Autowired
     private PersonRepository personaRepository;
     
+    @Transactional
     public void saveAllPersons(List<Persona> personas) {
     	personaRepository.saveAll(personas);
     }
 
+    @Transactional
     public void savePerson(Persona persona) {
         personaRepository.save(persona);
     }
